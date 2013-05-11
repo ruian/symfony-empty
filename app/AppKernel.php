7750054030514345cac2322ec5a4d8ef7b1b846f
Loic Chardonnet <loic.chardonnet@gmail.com>
2013-05-11 11:53:45 +0200

    Removed JMS dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,9 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
